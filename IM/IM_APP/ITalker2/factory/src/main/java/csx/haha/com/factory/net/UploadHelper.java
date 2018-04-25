30ca37e5d319fa0e4188d09b6365405f11e37df3
chenshixing <2677950307@qq.com>
2018-04-25 17:43:23 +0800

    APP 主界面 和Web 注册模块 对应的序列图和类图

--- IM/IM_APP/ITalker2/factory/src/main/java/csx/haha/com/factory/net/UploadHelper.java
+++ IM/IM_APP/ITalker2/factory/src/main/java/csx/haha/com/factory/net/UploadHelper.java
@@ -31,6 +31,7 @@ public class UploadHelper {
 
     private static OSS getClient() {
         // 明文设置secret的方式建议只在测试时使用，更多鉴权模式请参考后面的`访问控制`章节
+        //Access Key ID 和 Access Key Secret 记得修改！！！
         OSSCredentialProvider credentialProvider = new OSSPlainTextAKSKCredentialProvider(
                 "LTAIz1nLi7nT4Ryk", "ZiaMwnXDm2TFpUV3C1x41C29auozjZ");
         return new OSSClient(MyApplication.getInstance(), ENDPOINT, credentialProvider);
